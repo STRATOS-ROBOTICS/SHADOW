8b22995b90b23c8329a9805091f9daf139caa9ac
stratos robotics <55119049+STRATOS-ROBOTICS@users.noreply.github.com>
2022-02-15 18:45:15 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-//source infos
-//https://support.shadow.tech/hc/fr/articles/360008252593-Installation-de-l-application-Shadow-sur-Linux
+#//source infos
+#//https://support.shadow.tech/hc/fr/articles/360008252593-Installation-de-l-application-Shadow-sur-Linux
 
-//install dependancies
+#//install dependancies
 sudo apt update
 sudo apt install libva-glx2 libvdpau1 libva-drm2 libcurl4
 sudo usermod -a -G input $USER
 
-//download appimage if not already
-//wget https://update.shadow.tech/launcher/prod/linux/ubuntu_18.04/Shadow.AppImage
+#//download appimage if not already
+wget https://update.shadow.tech/launcher/prod/linux/ubuntu_18.04/Shadow.AppImage
 
-//set permissions
+#//set permissions
 sudo chmod +x Shadow.AppImage
 
 echo "OK"
